02c2385e73636a309081bc5f55ff52ce48276470
Kacper <kacper.mazur4@proton.me>
2024-02-23 17:39:47 +0100

    fix display time, delete unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,19 @@ import {useEffect, useState} from "react";
 
 function App() {
     const [city, setCity] = useState("Warsaw");
+    const [delayedCity, setDelayedCity] = useState(city);
     const [weatherData, setWeatherData] = useState(null);
+
     const apiURL = `https://api.weatherapi.com/v1/current.json?key=b8118ded294c42c1adc174209242202&q=${city}&aqi=no`;
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDelayedCity(city);
+        }, 500);
+
+        return () => clearTimeout(timerId);
+
+    }, [city]);
 
     useEffect(() => {
         fetch(apiURL)
@@ -17,37 +28,29 @@ function App() {
                 return response.json();
             })
             .then((data) => {
-                console.log(data);
                 setWeatherData(data);
+                console.log(data.current.isDay);
             })
             .catch((e) => {
                 console.log(e);
             })
-    }, [city]);
-
-
-    const stats = weatherData ? {
-        temp: weatherData.current.temp_c,
-        condition: weatherData.current.condition.text,
-        isDay: weatherData.current.isDay,
-        location: weatherData.location.name,
-        time: weatherData.location.localTime
-    } : {
-        temp: 0,
-        condition: '',
-        isDay: 0,
-        location: '',
-        time: ''
-    };
+    }, [delayedCity]);
 
 
   return (
     <div className='bg-[#1F213A] h-screen flex justify-center align-top'>
         <div className='mt-40 w-1/5 h-1/3'>
             <Temperature
-            setCity={setCity}
-            stats={stats}
+                setCity={setCity}
+                stats={{
+                    temp: weatherData?.current?.temp_c ?? 'Loading...',
+                    condition: weatherData?.current?.condition?.text ?? 'Loading...',
+                    isday: weatherData?.current?.isDay ?? 'Loading...',
+                    location: weatherData?.location?.name ?? 'Loading...',
+                    time: weatherData?.location?.localtime ?? 'Loading...'
+                }}
             />
+
         </div>
         <div className='mt-40 w-1/3 h-1/3 p-10 grid grid-cols-2 gap-6'>
             <h2 className='text-slate-200 text-2xl col-span-2'>Today's Highlights</h2>
